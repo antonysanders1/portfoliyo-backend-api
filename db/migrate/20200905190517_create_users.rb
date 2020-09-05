95519e9575e261f7dd4dbf3b819bb644bd65178d
antony_sanders <antony.devon92@gmail.com>
2020-09-05 16:23:45 -0400

    Created database

---
+++ db/migrate/20200905190517_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :title
+      t.text :bio
+      t.string :email
+      t.string :password_digest
+      t.integer :works_count, :integer, default: 0
+
+      t.timestamps
+    end
+  end
+end
