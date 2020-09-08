005a1628d866ac81b7aeb30990044caf95f3fe67
antony_sanders <antony.devon92@gmail.com>
2020-09-08 02:53:29 -0400

    created create & destroy sessions actions.. also created a currentUser action.. may not need TODO: refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,28 @@
 class SessionsController < ApplicationController
 
     def create
-        
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            created_jwt = issue_token({id: @user.id})
+            cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now}
+            render json: @user
+        else
+            render json: {
+                error: 'Oops! Email or Password Incorrect.'
+            }, status: 404
+        end
+    end
+
+    def destroy
+        cookies.delete(:jwt)
+    end
+
+    def currentUser
+        if logged_in?
+            render json: serializer_user(current_user)
+        else
+            render json: {error: "No current user"}
+        end
     end
 
 end
