a619fac571d3ce40dd5b903dcd839158e6f5d171
antony_sanders <antony.devon92@gmail.com>
2020-09-09 14:42:30 -0400

    added method to define user avatar url

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :title, :email, :bio
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :title, :email, :bio, :avatar
   has_many :works
+
+  def avatar
+    rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
+  end
 end
