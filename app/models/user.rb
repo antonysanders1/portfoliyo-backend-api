1e0ed06e0e1073f2e2dbfc2fa3a48601d2f38b51
antony_sanders <antony.devon92@gmail.com>
2020-09-09 14:40:48 -0400

    added method for user avatar url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+
     has_secure_password
     has_one_attached :avatar
     has_many :works
 
     validates :name, presence: true
     validates :email, uniqueness: true
+
+    def avatar_url
+        Rails.application.routes.url_helpers.rails_representation_url(
+          avatar.variant(resize_to_limit: [200, 200]).processed, only_path: true
+        )
+      end
     
 end
