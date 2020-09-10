74fbcdfc4a045543bda9db7e1e8c82908d79be9d
antony_sanders <antony.devon92@gmail.com>
2020-09-10 08:53:17 -0400

    added image url to serializer

--- app/serializers/work_serializer.rb
+++ app/serializers/work_serializer.rb
@@ -1,4 +1,9 @@
 class WorkSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :description, :image
   belongs_to :user
+
+  def image
+    rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
 end
